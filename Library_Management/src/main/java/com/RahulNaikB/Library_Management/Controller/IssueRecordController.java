bed7a214218a05b37081b54aacc72468f8ef58b6
B Rahul Naik <banavathrahulnaik26@gmail.com>
2025-06-11 23:46:33 +0530

    Updated BookController and IssueRecordController

---
+++ Library_Management/src/main/java/com/RahulNaikB/Library_Management/Controller/IssueRecordController.java
@@ -0,0 +1,12 @@
+package com.RahulNaikB.Library_Management.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/issuerecords")
+public class IssueRecordController
+{
+
+}
