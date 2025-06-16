8e6eb033bcf510a3e2d6e272319bf0c4df257655
B Rahul Naik <banavathrahulnaik26@gmail.com>
2025-06-16 22:30:31 +0530

    jwt token and jwt services add

--- Library_Management/src/main/java/com/RahulNaikB/Library_Management/JWT/JWTAuthenticationFilter.java
+++ Library_Management/src/main/java/com/RahulNaikB/Library_Management/JWT/JWTAuthenticationFilter.java
@@ -1,6 +1,94 @@
 package com.RahulNaikB.Library_Management.JWT;
 
-public class JWTAuthenticationFilter
-{
 
+import com.RahulNaikB.Library_Management.Repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter
+{
+    @Autowired
+    private final JwtService jwtService;
+
+
+    @Autowired
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException
+    {
+        final String authheader = request.getHeader("Authorization");
+        final String jwtToken;
+        final  String username;
+
+
+        //check if authorization header is present and starts with "Bearer"
+
+        if (authheader == null  || authheader.startsWith("Bearer"))
+        {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //Extract jwt Token From header
+
+        jwtToken = authheader.substring(7);
+        username = jwtService.extractUsername(jwtToken);
+
+        //Check if we have a username and no authentication exist yet
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null)
+        {
+            //Get the user details from database;
+
+            var userDetails = userRepository.findByUserName(username)
+                    .orElseThrow(()-> new RuntimeException("user not found"));
+
+
+            //Validate the Token
+
+            if(jwtService.isTokenValid (jwtToken, userDetails))
+            {
+
+                //create the authentication with user roles
+                List<SimpleGrantedAuthority > authorities = userDetails.getRoles()
+                        .stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null,authorities);
+
+
+                //set authentication details
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+
+                //update Security Context Authentication
+                SecurityContextHolder.getContext().getAuthentication();
+
+
+            }
+        }
+        filterChain.doFilter(request, response);
+
+    }
 }
--- Library_Management/src/main/java/com/RahulNaikB/Library_Management/JWT/JwtService.java
+++ Library_Management/src/main/java/com/RahulNaikB/Library_Management/JWT/JwtService.java
@@ -1,10 +1,73 @@
 package com.RahulNaikB.Library_Management.JWT;
 
-
+import com.RahulNaikB.Library_Management.Entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-@Service
-public class JwtService
-{
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+@Service
+public class JwtService {
+
+    @Value("${jwt.secretkey}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration; // in milliseconds
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String extractUsername(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String username = extractUsername(jwtToken);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwtToken);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
